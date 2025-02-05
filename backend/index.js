734af71f2a58054029d63fb04f5702ce9321158d
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-05 15:51:59 +0100

    set up user route

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
+const authRouter = require('./routes/auth_routes');
+
+require('dotenv').config();
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors({ origin: `http://localhost:5173`, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-const PORT = process.env.PORT || 5000;
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
