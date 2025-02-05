5f44df4cec9eee98e1cc1ef7977d6a1d146835d5
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-05 15:53:04 +0100

    middleware to veirify if a user is authenticated to do a specific action

---
+++ backend/middlewares/verifyToken.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ success: false, message: 'Not authenticated' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res.status(401).json({ success: false, message: 'Invalid token' });
+    }
+
+    req.userID = decoded.userID;
+
+    next();
+  } catch {
+    console.error('Error in verifyToken', error);
+    return res.status(500).json({ success: false, message: 'Invalid token' });
+  }
+};
+
+module.exports = verifyToken;
