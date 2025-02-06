b5c4bec79eefef7abc9ad9c134d3b17b758177d5
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-06 16:17:21 +0100

    create the checkAuth route & add the verifyToken middleware

--- backend/routes/auth_routes.js
+++ backend/routes/auth_routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
 const authController = require('../controllers/user/index');
+const verifyToken = require('../middlewares/verifyToken');
 
 const userRouter = express.Router();
+
+userRouter.get('/check-auth', verifyToken, authController.checkAuth);
 
 userRouter.post('/signup', authController.signup);
 userRouter.post('/login', authController.login);
