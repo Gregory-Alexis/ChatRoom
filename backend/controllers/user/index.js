36728659a4134cabf06fe7723c20c2cf4d5e4c95
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-06 16:16:48 +0100

    import checkAuth controller in the main export file

--- backend/controllers/user/index.js
+++ backend/controllers/user/index.js
@@ -1,5 +1,6 @@
+const checkAuth = require('./checkAuth');
 const login = require('./login');
 const logout = require('./logout');
 const signup = require('./signup');
 
-module.exports = { signup, login, logout };
+module.exports = { signup, login, logout, checkAuth };
