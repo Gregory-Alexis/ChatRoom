45b8d039d2dec216b319142617e91b0e7e686669
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-06 16:07:14 +0100

    import login & logout controller in the mail export file

--- backend/controllers/user/index.js
+++ backend/controllers/user/index.js
@@ -1,3 +1,5 @@
+const login = require('./login');
+const logout = require('./logout');
 const signup = require('./signup');
 
-module.exports = { signup };
+module.exports = { signup, login, logout };
