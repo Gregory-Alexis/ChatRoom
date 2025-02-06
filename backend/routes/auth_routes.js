cd1c913b5cc635159f39c1055474b63e516a9030
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-06 16:08:12 +0100

    import loign & logout controllers and create there endpoint route

--- backend/routes/auth_routes.js
+++ backend/routes/auth_routes.js
@@ -5,5 +5,7 @@ const authController = require('../controllers/user/index');
 const userRouter = express.Router();
 
 userRouter.post('/signup', authController.signup);
+userRouter.post('/login', authController.login);
+userRouter.post('/logout', authController.logout);
 
 module.exports = userRouter;
