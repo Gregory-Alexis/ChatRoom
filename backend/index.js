0a6d7e2f8f4a32e8cbc3e091e04e72da13887b07
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-06 15:37:01 +0100

    socket.io backend config

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const { createServer } = require('node:http');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const { Server } = require('socket.io');
 
 const authRouter = require('./routes/auth_routes');
 
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const server = createServer(app);
+const PORT = process.env.PORT || 5001;
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+});
 
 app.use(cors({ origin: `http://localhost:5173`, credentials: true }));
 app.use(express.json());
@@ -15,6 +25,18 @@ app.use(cookieParser());
 
 app.use('/api/auth', authRouter);
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on('disconnect', () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+
+  socket.on('error', (err) => {
+    console.error(`Socket error: ${err.message}`);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
