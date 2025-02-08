db4a8a53897fd1332f7dbca9cda2fb5f03b4c015
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-08 17:12:51 +0100

    change create endpoint name & add delete post endpoint

--- backend/routes/post_routes.js
+++ backend/routes/post_routes.js
@@ -5,6 +5,8 @@ const verifyToken = require('../middlewares/verifyToken');
 
 const postRouter = express.Router();
 
-postRouter.post('/create-post', verifyToken, postController.createPost);
+postRouter.get('/', postController.getPosts);
+postRouter.post('/create', verifyToken, postController.createPost);
+postRouter.delete('/delete/:postID', verifyToken, postController.deletePost);
 
 module.exports = postRouter;
