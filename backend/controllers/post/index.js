6085f1d7c90320091906a180b2d0234838947637
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-09 12:54:10 +0100

    import likepost to the main export fil

--- backend/controllers/post/index.js
+++ backend/controllers/post/index.js
@@ -1,6 +1,7 @@
 const createPost = require('./createPost');
 const deletePost = require('./deletePost');
 const getPosts = require('./getPost');
+const likePost = require('./likePost');
 const updatePost = require('./updatePost');
 
-module.exports = { createPost, getPosts, deletePost, updatePost };
+module.exports = { createPost, getPosts, deletePost, updatePost, likePost };
