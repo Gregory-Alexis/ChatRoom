d8b3f8235c06c6e7a3b412f6ddc80052a915e502
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-09 10:54:51 +0100

    import then export updatepost controller

--- backend/controllers/post/index.js
+++ backend/controllers/post/index.js
@@ -1,5 +1,6 @@
 const createPost = require('./createPost');
 const deletePost = require('./deletePost');
 const getPosts = require('./getPost');
+const updatePost = require('./updatePost');
 
-module.exports = { createPost, getPosts, deletePost };
+module.exports = { createPost, getPosts, deletePost, updatePost };
