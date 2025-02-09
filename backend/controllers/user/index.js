5ad4eeb06a1193661ef6424e791d24dd0e592e74
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-09 12:17:27 +0100

    import deleteUser controller to the mail import file

--- backend/controllers/user/index.js
+++ backend/controllers/user/index.js
@@ -1,6 +1,7 @@
 const checkAuth = require('./checkAuth');
+const deleteUser = require('./deleteUser');
 const login = require('./login');
 const logout = require('./logout');
 const signup = require('./signup');
 
-module.exports = { signup, login, logout, checkAuth };
+module.exports = { signup, login, logout, checkAuth, deleteUser };
