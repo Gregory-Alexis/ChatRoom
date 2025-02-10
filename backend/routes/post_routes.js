0b333e6bac18511fddc812f511d5b965dc95178e
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-10 15:26:10 +0100

    add dislike post endpoint  route

--- backend/routes/post_routes.js
+++ backend/routes/post_routes.js
@@ -6,9 +6,13 @@ const verifyToken = require('../middlewares/verifyToken');
 const postRouter = express.Router();
 
 postRouter.get('/', postController.getPosts);
+
 postRouter.post('/create', verifyToken, postController.createPost);
 postRouter.post('/create/like-post/:postID', verifyToken, postController.likePost);
+postRouter.post('/create/dislike-post/:postID', verifyToken, postController.dislikePost);
+
 postRouter.delete('/delete/:postID', verifyToken, postController.deletePost);
+
 postRouter.put('/update/:postID', verifyToken, postController.updatePost);
 
 module.exports = postRouter;
