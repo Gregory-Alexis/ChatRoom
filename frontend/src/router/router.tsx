3316e059a1ea395f07b2e1237b9ed6d439f9c1cc
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-10 16:09:03 +0100

    setup the frontend routes

---
+++ frontend/src/router/router.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import Layout from '../layout/Layout';
+import HomePage from '../pages/HomePage';
+import LoginPage from '../pages/LoginPage';
+import SignUpPage from '../pages/SignUpPage';
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/signup' element={<SignUpPage />} />
+    </Route>
+  )
+);
