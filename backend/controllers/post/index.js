20b2a828a30000e8aef9d9959e68f7e5c62dc9e5
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-10 15:24:55 +0100

    add dislike post controller to the main export file

--- backend/controllers/post/index.js
+++ backend/controllers/post/index.js
@@ -1,7 +1,8 @@
 const createPost = require('./createPost');
 const deletePost = require('./deletePost');
+const dislikePost = require('./dislikePost');
 const getPosts = require('./getPost');
 const likePost = require('./likePost');
 const updatePost = require('./updatePost');
 
-module.exports = { createPost, getPosts, deletePost, updatePost, likePost };
+module.exports = { createPost, getPosts, deletePost, updatePost, likePost, dislikePost };
