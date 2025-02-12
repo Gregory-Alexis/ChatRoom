0ff4768bce542ab8821b06e9e75bef5f7f8e3da8
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-12 18:51:37 +0100

    wrapped up LoginPage and SignUpPage with the  RedirectAuthenticatedUser component

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -3,13 +3,28 @@ import Layout from '../layout/Layout';
 import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/LoginPage';
 import SignUpPage from '../pages/SignUpPage';
+import RedirectAuthenticatedUser from '../components/RedirectAuthenticatedUser';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
       <Route index element={<HomePage />} />
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/signup' element={<SignUpPage />} />
+      <Route
+        path='/login'
+        element={
+          <RedirectAuthenticatedUser>
+            <LoginPage />
+          </RedirectAuthenticatedUser>
+        }
+      />
+      <Route
+        path='/signup'
+        element={
+          <RedirectAuthenticatedUser>
+            <SignUpPage />
+          </RedirectAuthenticatedUser>
+        }
+      />
     </Route>
   )
 );
