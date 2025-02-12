e4202047be0bd43dac2d20a1c3f51ad4cdc581ac
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-12 18:48:39 +0100

    defined type action store

--- frontend/src/models/AuthStore.ts
+++ frontend/src/models/AuthStore.ts
@@ -6,4 +6,7 @@ export type AuthStore = {
   error: string | null;
   isAuthenticated: boolean;
   isCheckingAuth: boolean;
+  signup: (username: string, email: string, password: string) => Promise<void>;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
 };
