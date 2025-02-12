bece569c6db726a168b0e5a60c9e2bfffbf809e4
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-12 18:48:26 +0100

    create login, logout & check_auth actions store

--- frontend/src/store/authStore.ts
+++ frontend/src/store/authStore.ts
@@ -35,4 +35,68 @@ export const useAuthStore = create<AuthStore>((set) => ({
       throw error;
     }
   },
+
+  login: async (email: string, password: string) => {
+    set({ isLoading: true });
+
+    try {
+      const response = await axios.post(`${API_URL}/login`, {
+        email,
+        password,
+      });
+
+      set({ user: response.data.user, isAuthenticated: true, isLoading: false, error: null });
+    } catch (error: unknown) {
+      let errorMessage = 'An unexpected error occurred';
+
+      if (axios.isAxiosError(error)) {
+        errorMessage = error.response?.data?.message || 'Something went wrong';
+      } else if (error instanceof Error) {
+        errorMessage = error.message;
+      }
+
+      set({ error: errorMessage, isLoading: false });
+      throw error;
+    }
+  },
+
+  logout: async () => {
+    set({ isAuthenticated: false, isLoading: true });
+
+    try {
+      await axios.post(`${API_URL}/logout`);
+      set({ isAuthenticated: false, user: null, isLoading: false, error: null });
+    } catch (error: unknown) {
+      let errorMessage = 'An unexpected error occurred';
+
+      if (axios.isAxiosError(error)) {
+        errorMessage = error.response?.data?.message || 'Something went wrong';
+      } else if (error instanceof Error) {
+        errorMessage = error.message;
+      }
+
+      set({ error: errorMessage, isLoading: false });
+      throw error;
+    }
+  },
+
+  checkAuth: async () => {
+    set({ isCheckingAuth: false });
+
+    try {
+      const response = await axios.get(`${API_URL}/check-auth`);
+      set({ user: response.data.user, isAuthenticated: true, isCheckingAuth: true });
+    } catch (error: unknown) {
+      let errorMessage = 'An unexpected error occurred';
+
+      if (axios.isAxiosError(error)) {
+        errorMessage = error.response?.data?.message || 'Something went wrong';
+      } else if (error instanceof Error) {
+        errorMessage = error.message;
+      }
+
+      set({ error: errorMessage, isLoading: false });
+      throw error;
+    }
+  },
 }));
