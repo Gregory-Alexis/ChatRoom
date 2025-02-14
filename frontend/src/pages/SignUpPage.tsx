616c6faf9e2c15aed74a08563edfb84b0c7bf18a
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-14 14:58:34 +0100

    create signup page and handle the form with react-hook-form

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,6 +1,32 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import { useAuthStore } from '../store/authStore';
+import axios from 'axios';
+
+type SignUpInput = {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
 
 const SignUpPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpInput>();
+
+  const { signup } = useAuthStore();
+
+  const SubmitHandler: SubmitHandler<SignUpInput> = async (data) => {
+    try {
+      await signup(data.username, data.email, data.password);
+    } catch (error: any) {
+      throw new Error(`Error signing up: ${error.message}`);
+    }
+  };
+
   return (
     <div className='p-6 h-screen flex flex-col justify-center items-center'>
       <div className='text-center mb-8'>
@@ -8,7 +34,10 @@ const SignUpPage = () => {
         <p className='text-lg text-gray-700 mt-2'>Join the community</p>
       </div>
 
-      <form className='bg-white p-8 rounded-lg shadow-md w-full max-w-md'>
+      <form
+        onSubmit={handleSubmit(SubmitHandler)}
+        className='bg-white p-8 rounded-lg shadow-md w-full max-w-md'
+      >
         <div className='mb-6'>
           <label htmlFor='username' className='block text-gray-700 font-semibold mb-2'>
             Username
@@ -18,8 +47,13 @@ const SignUpPage = () => {
             id='username'
             placeholder='John Doe'
             className='w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-500'
+            {...register('username', {
+              required: 'A username is required',
+            })}
           />
         </div>
+
+        {errors.username && <p className='text-red-500'>{errors.username.message}</p>}
 
         <div className='mb-6'>
           <label htmlFor='email' className='block text-gray-700 font-semibold mb-2'>
@@ -30,8 +64,13 @@ const SignUpPage = () => {
             id='email'
             placeholder='John_Doe@yourmail.com'
             className='w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-500'
+            {...register('email', {
+              required: 'An email is required',
+            })}
           />
         </div>
+
+        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
 
         <div className='mb-6'>
           <label htmlFor='password' className='block text-gray-700 font-semibold mb-2'>
@@ -42,8 +81,13 @@ const SignUpPage = () => {
             id='password'
             placeholder='Your password'
             className='w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-500'
+            {...register('password', {
+              required: 'A password is required',
+            })}
           />
         </div>
+
+        {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
 
         <div className='mb-6'>
           <label htmlFor='confirmPassword' className='block text-gray-700 font-semibold mb-2'>
@@ -54,8 +98,14 @@ const SignUpPage = () => {
             id='confirmPassword'
             placeholder='Confirm your password'
             className='w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-500'
+            {...register('confirmPassword', {
+              required:
+                'You need to confirm your password. If no password was provided, please provide one',
+            })}
           />
         </div>
+
+        {errors.confirmPassword && <p className='text-red-500'>{errors.confirmPassword.message}</p>}
 
         <button
           type='submit'
