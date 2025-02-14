9f40ab1c2587382fa8acc6326d8ca75ab359bb39
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-14 14:58:11 +0100

    create login Page and handle the form with react-hook-form

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,6 +1,29 @@
 import { Link } from 'react-router-dom';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { useAuthStore } from '../store/authStore';
+
+type LoginInput = {
+  email: string;
+  password: string;
+};
 
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginInput>();
+
+  const { login } = useAuthStore();
+
+  const LoginHandler: SubmitHandler<LoginInput> = async (data) => {
+    try {
+      await login(data.email, data.password);
+    } catch (error: any) {
+      throw new Error(`Error logging in: ${error.message}`);
+    }
+  };
+
   return (
     <div className='p-6 min-h-screen flex flex-col justify-center items-center'>
       <div className='text-center mb-8'>
@@ -8,7 +31,10 @@ const LoginPage = () => {
         <p className='text-lg text-gray-800 mt-2'>Login and chat with friends</p>
       </div>
 
-      <form className='bg-white p-8 rounded-lg shadow-md w-full max-w-md'>
+      <form
+        onSubmit={handleSubmit(LoginHandler)}
+        className='bg-white p-8 rounded-lg shadow-md w-full max-w-md'
+      >
         <div className='mb-6'>
           <label htmlFor='email' className='block text-gray-700 font-semibold mb-2'>
             Email
@@ -18,9 +44,18 @@ const LoginPage = () => {
             id='email'
             placeholder='Enter your email'
             className='w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-500'
+            {...register('email', {
+              required: 'Email is required',
+              pattern: {
+                value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                message: 'Invalid email format',
+              },
+            })}
             required
           />
         </div>
+
+        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
 
         <div className='mb-6'>
           <label htmlFor='password' className='block text-gray-700 font-semibold mb-2'>
@@ -31,9 +66,13 @@ const LoginPage = () => {
             id='password'
             placeholder='Enter your password'
             className='w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-500'
-            required
+            {...register('password', {
+              required: 'Password is required',
+            })}
           />
         </div>
+
+        {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
 
         <button
           type='submit'
@@ -52,4 +91,5 @@ const LoginPage = () => {
     </div>
   );
 };
+
 export default LoginPage;
